05b7a327642e42cc7670c8119796d9c18a462376
Jason Gu <1057337859@qq.com>
2022-07-04 18:46:04 -0400

    添加了淡化管理器，方便管理可淡化的物体

---
+++ Assets/scripts/ExtraFrame/FaderMgr.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 可淡化物体接口
+/// </summary>
+public interface IFader
+{
+    public void FadeIn(string objectName);  //淡入
+    public void FadeOut(string objectName); //淡出
+    
+}
+
+
+public class FaderMgr : SingletonMono<FaderMgr>
+{
+    /// <summary>
+    /// 所有可淡化物体的字典
+    /// 键：物体名
+    /// 值：物体脚本
+    /// </summary>
+    private Dictionary<string, IFader> faderDict = new Dictionary<string, IFader>();
+
+    private void Awake()
+    {
+        //添加对于物体淡化的监听
+        EventMgr.Instance.AddEventListener<string>("TriggerFadeIn", TriggerFadeIn); 
+        EventMgr.Instance.AddEventListener<string>("TriggerFadeOut", TriggerFadeOut);
+    }
+
+    /// <summary>
+    /// 登记可淡化物体
+    /// </summary>
+    /// <param name="objectName">物体名</param>
+    /// <param name="obj">物体脚本</param>
+    public void AddFaderRegistry(string objectName, IFader obj)
+    {
+        faderDict.Add(objectName,obj);
+    }
+    
+    /// <summary>
+    /// 触发对应物体的淡入
+    /// </summary>
+    /// <param name="objectName">物体名</param>
+    private void TriggerFadeIn(string objectName)
+    {
+        if (faderDict.ContainsKey(objectName))
+        {
+            faderDict[objectName].FadeIn(objectName);
+        }
+    }
+    /// <summary>
+    /// 触发对应物体的淡出
+    /// </summary>
+    /// <param name="objectName">物体名</param>
+    private void TriggerFadeOut(string objectName)
+    {
+        if (faderDict.ContainsKey(objectName))
+        {
+            faderDict[objectName].FadeOut(objectName);
+        }
+    }
+    
+    /// <summary>
+    /// 清空字典，切场景使用
+    /// </summary>
+    public void Clear()
+    {
+        faderDict.Clear();
+    }
+}
--- Assets/scripts/SceneObjects/SceneTree.cs
+++ Assets/scripts/SceneObjects/SceneTree.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using DG.Tweening;
 using TMPro;
 
-public class SceneTree : MonoBehaviour
+public class SceneTree : MonoBehaviour, IFader
 {
     private SpriteRenderer[] spriteRenderersInChildren;
     private Collider2D treeTrigger;
@@ -15,19 +15,15 @@ public class SceneTree : MonoBehaviour
         spriteRenderersInChildren = GetComponentsInChildren<SpriteRenderer>();
         treeTrigger = GetComponent<Collider2D>();
         
-        EventMgr.Instance.AddEventListener<Collider2D>("TriggerFadeIn", FadeIn);
-        EventMgr.Instance.AddEventListener<Collider2D>("TriggerFadeOut", FadeOut);
-
-
+        FaderMgr.Instance.AddFaderRegistry(this.gameObject.name, this); //向管理器登记自己 （每次换场景也要登记）
     }
 
 
     /// <summary>
     /// 淡出
     /// </summary>
-    public void FadeOut(Collider2D tree)
+    public void FadeOut(string treeName)
     {
-        if (tree != treeTrigger) return;
         Color fadeColor = new Color(1f, 1f, 1f, 1f);
         foreach (var sr in spriteRenderersInChildren)
         {
@@ -38,9 +34,8 @@ public class SceneTree : MonoBehaviour
     /// <summary>
     /// 淡入
     /// </summary>
-    public void FadeIn(Collider2D tree)
+    public void FadeIn(string treeName)
     {
-        if (tree != treeTrigger) return;
         Color fadeColor = new Color(1f, 1f, 1f, Settings.fadeAlpha);
         foreach (var sr in spriteRenderersInChildren)
         {
