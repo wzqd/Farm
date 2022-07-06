d1d53bb34690c76417e975672e9d3ce7d903cba4
Jason Gu <1057337859@qq.com>
2022-07-05 21:41:54 -0400

    重新整理文件

---
+++ Assets/Editor/UIBuilder/ItemEditor.cs
@@ -0,0 +1,29 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+
+public class ItemEditor : EditorWindow
+{
+    [MenuItem("CustomEditors/ItemEditor")]
+    public static void ShowExample()
+    {
+        ItemEditor wnd = GetWindow<ItemEditor>();
+        wnd.titleContent = new GUIContent("ItemEditor");
+    }
+
+    public void CreateGUI()
+    {
+        // Each editor window contains a root VisualElement object
+        VisualElement root = rootVisualElement;
+        
+
+        // Import UXML
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UIBuilder/ItemEditor.uxml");
+        VisualElement labelFromUXML = visualTree.Instantiate();
+        root.Add(labelFromUXML);
+
+
+    }
+}
